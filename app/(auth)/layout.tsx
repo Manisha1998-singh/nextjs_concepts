eca13e892bbc858268f12aeea8c6b70d58a7d8f8
Manisha Singh <manishasinghbca89@gmail.com>
2026-06-25 05:29:37 +0530

    Worked on params and searchParams.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,20 +1,45 @@
-export const metadata = {
-  title: "Next.js",
-  description: "Generate by Next.js",
-};
+"use client";
+import Link from "next/link";
+import "./styles.css";
+//current active link
+import { usePathname } from "next/navigation";
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forget Password", href: "/forget-password" },
+];
+// export const metadata = {
+//   title: "Next.js",
+//   description: "Generate by Next.js",
+// };
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   return (
-    <html lang="en">
-      <body>
-        <header style={{ backgroundColor: "lightblue", padding: "1rem" }}>
-          <p>Header</p>
-        </header>
-        <div>{children}</div>
-      </body>
-    </html>
+    <>
+      <div>
+        {navLinks.map((link) => {
+          const isActive =
+            pathname === link.href ||
+            (pathname.startsWith(link.href) && link.href !== "/");
+          return (
+            <Link
+              href={link.href}
+              key={link.name}
+              className={
+                isActive
+                  ? "font-bold mr-4 bg-amber-500"
+                  : "text-blue-500 mr-4 bg-amber-800"
+              }>
+              {link.name}
+            </Link>
+          );
+        })}
+        {children}
+      </div>
+    </>
   );
 }
