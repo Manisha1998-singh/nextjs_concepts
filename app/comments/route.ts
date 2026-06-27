ee18ea4b9d5bd56916f2ab0d4421c65a86069b95
Manisha Singh <manishasinghbca89@gmail.com>
2026-06-28 03:27:53 +0530

    worked on URL Query Parameters.

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,7 +1,14 @@
 import { comments } from "./data";
+import { type NextRequest } from "next/server";
 
-export async function GET() {
-  return Response.json(comments);
+export async function GET(request: NextRequest) {
+  //http://localhost:3000/comments?query=first
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query"); // you can create multiple query like this
+  const filterComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filterComments);
 }
 export async function POST(request: Request) {
   const comment = await request.json();
