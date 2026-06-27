38355a4637fbe75f7e1e62f04b27d9d7dddcfe3f
Manisha Singh <manishasinghbca89@gmail.com>
2026-06-28 05:19:05 +0530

    Worked on response header.

--- app/profiles/Api/route.ts
+++ app/profiles/Api/route.ts
@@ -1,7 +1,17 @@
 //How to read headers
 import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
 export async function GET(request: NextRequest) {
-  const requestHeaders = new Headers(request.headers);
-  console.log(requestHeaders.get("authorization"));
-  return new Response("Profile API Data");
+  // const requestHeaders = new Headers(request.headers);
+  // console.log(requestHeaders.get("authorization"));
+
+  const headerList = await headers();
+  console.log(headerList.get("Authorization"));
+  //return new Response("Profile API Data");
+  // return new Response("<h1>Profile API Data</h1>"); //not work
+  return new Response("<h1>Profile API Data</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  });
 }
