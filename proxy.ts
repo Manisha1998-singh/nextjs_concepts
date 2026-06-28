ad14a6ce71d07a00922d88f9117ee38ff84de2a7
Manisha Singh <manishasinghbca89@gmail.com>
2026-06-29 04:56:50 +0530

    Worked on headers in middleware.

--- proxy.ts
+++ proxy.ts
@@ -1,15 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  console.log("Middleware running:", request.nextUrl.pathname);
+export function proxy(request: NextRequest) {
+  console.log("Proxy running:", request.nextUrl.pathname);
 
   const response = NextResponse.next();
+
   const themePreference = request.cookies.get("theme");
+
   if (!themePreference) {
     response.cookies.set("theme", "dark");
   }
-  //response.cookies.set("theme", "dark");
-  //response.headers.set("custom-header", "custom-value");
+
+  response.headers.set("custom-header", "custom-value");
 
   return response;
 }
