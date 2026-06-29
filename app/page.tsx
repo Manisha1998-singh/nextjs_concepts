af2b9294f77fa5a6ba6804595e7bd06a7aee2b2f
Manisha Singh <manishasinghbca89@gmail.com>
2026-06-29 18:54:25 +0530

    Worked on server and client components.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
+      <h1>Dashboard </h1>
       <h1>Welcome Home</h1>
+      <Link href="/dashboard_client">Dashboard</Link>
       <Link href="/blog">Blog</Link>
       <Link href="/products">Products</Link>
       <Link href="/articles/breaking-news-123?lang=en">Read in English</Link>
