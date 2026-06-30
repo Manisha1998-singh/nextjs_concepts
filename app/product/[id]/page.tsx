31e9190e2fe7ba4ac19d912a05ad2010da05422f
Manisha Singh <manishasinghbca89@gmail.com>
2026-06-30 18:52:47 +0530

    Worked on generateStaticParams.

---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export async function generatStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
+export default async function ProPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  return (
+    <div>
+      <h1>
+        Products {id} details rendered at {new Date().toLocaleTimeString()}
+      </h1>
+    </div>
+  );
+}
