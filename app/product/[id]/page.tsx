913f0aa1cdd93961759199c9aad4f57cc0803985
Manisha Singh <manishasinghbca89@gmail.com>
2026-06-30 19:07:12 +0530

    Worked on dynamicParams.

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-
-export async function generatStaticParams() {
+export const dynamicParams = false;
+export async function generateStaticParams() {
   return [{ id: "1" }, { id: "2" }, { id: "3" }];
 }
 export default async function ProPage({
