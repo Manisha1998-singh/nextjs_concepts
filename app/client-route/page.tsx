d46766fe48dcfa5590e44600124462da2ea48733
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-02 04:20:37 +0530

    Worked on client only code.

--- app/client-route/page.tsx
+++ app/client-route/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { useTheme } from "../components/theme-provider";
-
+import { clientSideFunction } from "../utils/client-utils";
 export default function ClientRoutePage() {
   const theme = useTheme();
-
+  const result = clientSideFunction();
   return (
     <>
       <div style={{ color: theme.colors.primary }}>Client Route Page </div>
+      <p>{result}</p>
     </>
   );
 }
