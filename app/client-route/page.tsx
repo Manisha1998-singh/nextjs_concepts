4c3f03e1ab5cce83181582086fcebfcd7025b4b1
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-02 03:24:39 +0530

    Worked on context Provider.

--- app/client-route/page.tsx
+++ app/client-route/page.tsx
@@ -1,18 +1,8 @@
 "use client";
 import { useTheme } from "../components/theme-provider";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 export default function ClientRoutePage() {
   const theme = useTheme();
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
 
   return (
     <>
